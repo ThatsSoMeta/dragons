88278d8f21b6e9601ff5baf6c9282261919c9c80
CG364 <isaiahtako@gmail.com>
2021-04-06 16:13:12 -0400

    started on the character sheet models

--- characters/models.py
+++ characters/models.py
@@ -1,10 +1,24 @@
 from django.db import models
 
 # Create your models here.
+# Trying to find a more DRY way to do the stats
 
-
-class CustomCharacter(models.Model):
+class CharacterSheet(models.Model):
     name = models.CharField(max_length=50)
-    creator = models.ForeignKey('DragonUser')
-    concept = models.CharField(max_length=280)
+    player = models.ForeignKey('DragonUser')
     level = models.IntegerField(default=1)
+    race = models.CharField(max_length=20)
+    alignment = models.CharField(max_length=25)
+    background = models.CharField(max_length=25)
+
+class SpellSheet(models.Model):
+    spell_name = models.CharField(max_length=60)
+    spell_type = models.CharField(max_length=40)
+    casting_time = models.CharField(max_length=25)
+    spell_range = models.IntegerField()
+    spell_components = models.CharField(max_length=200)
+    spell_duration = models.IntegerField()
+    spell_description = models.CharField(max_length=200)
+
+class CharacterDescriptions(models.Model):
+    ...
